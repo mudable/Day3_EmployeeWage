0757d9087adc8eeb36b056bc8f02178278d4163d
mudable <mudableshilpa@gmail.com>
2021-10-20 13:31:23 +0530

    Added:UC1 To check employee is present or absent

--- src/com/bz_employeewage/EmployeeWage.java
+++ src/com/bz_employeewage/EmployeeWage.java
@@ -3,7 +3,12 @@ package com.bz_employeewage;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to EmPloyee Wage Program"); 
+		System.out.println("Welcome to EmPloyee Wage Program");
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
