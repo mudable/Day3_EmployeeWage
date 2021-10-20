cbe09b7cdcd537d648182b71a100eea8be702000
mudable <mudableshilpa@gmail.com>
2021-10-20 13:58:45 +0530

    Added:UC3 to calculate employee wage for part time

--- src/com/bz_employeewage/EmployeeWage.java
+++ src/com/bz_employeewage/EmployeeWage.java
@@ -13,10 +13,13 @@ public class EmployeeWage {
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME) {
 			empHrs = 8;
-			System.out.println("Employee is Present ");
+			System.out.println("Employee is Present full time ");
+		} else if (empCheck == IS_PART_TIME) {
+			empHrs = 4;
+			System.out.println("Employee is present part time");
 		} else {
 			empHrs = 0;
-			System.out.println("Employee is Absent");
+			System.out.println("Employee is absent");
 		}
 		int empWage = empHrs * RATE_PER_HR;
 		System.out.println("Emp Wage: " + empWage);
