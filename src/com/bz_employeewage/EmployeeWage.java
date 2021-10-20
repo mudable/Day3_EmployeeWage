d6ce8896cbfb9933d0e121c61e709fb1b92ad54c
mudable <mudableshilpa@gmail.com>
2021-10-20 16:32:26 +0530

    Added:UC5 to calculate total employee wage

--- src/com/bz_employeewage/EmployeeWage.java
+++ src/com/bz_employeewage/EmployeeWage.java
@@ -7,25 +7,32 @@ public class EmployeeWage {
 		int FULL_DAY_HR = 8;
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 0;
-		System.out.println("Welcome to EmPloyee Wage Program");
-		int empCheck = (int) (Math.random() * 10) % 2;
+		int NO_OF_WORKING_DAYS = 20;
+		int totalEmpWage = 0;
 		int empHrs = 0;
-		switch (empCheck) {
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
+		System.out.println("Welcome to EmPloyee Wage Program");
 
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
+		for (int i = 0; i < NO_OF_WORKING_DAYS; i++) {
+			int empCheck = (int) (Math.random() * 10) % 2;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
 
-		default:
-			empHrs = 0;
-			break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
 
+			default:
+				empHrs = 0;
+				break;
+
+			}
+			int empWage = empHrs * RATE_PER_HR;
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("Emp Wage: " + empWage);
+			System.out.println("Total Emp Wage:" + totalEmpWage);
 		}
-		int empWage = empHrs * RATE_PER_HR;
-		System.out.println("Emp Wage: " + empWage);
 
 	}
 }
