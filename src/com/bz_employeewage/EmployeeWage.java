fd2f5c9ab25ff2352c5ab6eb68928e9eff74876c
mudable <mudableshilpa@gmail.com>
2021-10-20 16:16:55 +0530

    Added:UC4 Switch case statement

--- src/com/bz_employeewage/EmployeeWage.java
+++ src/com/bz_employeewage/EmployeeWage.java
@@ -5,23 +5,27 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		int RATE_PER_HR = 20;
 		int FULL_DAY_HR = 8;
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 0;
-
-		int empHrs = 0;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 0;
 		System.out.println("Welcome to EmPloyee Wage Program");
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-			System.out.println("Employee is Present full time ");
-		} else if (empCheck == IS_PART_TIME) {
+		int empCheck = (int) (Math.random() * 10) % 2;
+		int empHrs = 0;
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-			System.out.println("Employee is present part time");
-		} else {
+			break;
+
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+
+		default:
 			empHrs = 0;
-			System.out.println("Employee is absent");
+			break;
+
 		}
 		int empWage = empHrs * RATE_PER_HR;
 		System.out.println("Emp Wage: " + empWage);
+
 	}
 }
