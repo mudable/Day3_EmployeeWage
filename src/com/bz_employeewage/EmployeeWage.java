aaab0a1007213d277c6e109f9b6478b87b205a31
mudable <mudableshilpa@gmail.com>
2021-10-20 13:52:35 +0530

    Added:UC2 to calculate employee wage

--- src/com/bz_employeewage/EmployeeWage.java
+++ src/com/bz_employeewage/EmployeeWage.java
@@ -3,12 +3,22 @@ package com.bz_employeewage;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
+		int RATE_PER_HR = 20;
+		int FULL_DAY_HR = 8;
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 0;
+
+		int empHrs = 0;
 		System.out.println("Welcome to EmPloyee Wage Program");
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == 1)
-			System.out.println("Employee is Present");
-		else
+		if (empCheck == IS_FULL_TIME) {
+			empHrs = 8;
+			System.out.println("Employee is Present ");
+		} else {
+			empHrs = 0;
 			System.out.println("Employee is Absent");
+		}
+		int empWage = empHrs * RATE_PER_HR;
+		System.out.println("Emp Wage: " + empWage);
 	}
-
 }
