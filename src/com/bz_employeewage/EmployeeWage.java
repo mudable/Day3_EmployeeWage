0f1e9288d6a3aaf99a1c4ad8ed409f6c7095de6c
mudable <mudableshilpa@gmail.com>
2021-10-20 16:53:14 +0530

    Added:UC6

--- src/com/bz_employeewage/EmployeeWage.java
+++ src/com/bz_employeewage/EmployeeWage.java
@@ -8,11 +8,16 @@ public class EmployeeWage {
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 0;
 		int NO_OF_WORKING_DAYS = 20;
+		int MAX_WORKING_HR = 100;
 		int totalEmpWage = 0;
 		int empHrs = 0;
+		int totalEmpHr = 0;
+		int totalWorkingDays = 0;
 		System.out.println("Welcome to EmPloyee Wage Program");
 
-		for (int i = 0; i < NO_OF_WORKING_DAYS; i++) {
+		while (totalEmpHr < MAX_WORKING_HR && totalWorkingDays < NO_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+
 			int empCheck = (int) (Math.random() * 10) % 2;
 			switch (empCheck) {
 			case IS_PART_TIME:
@@ -28,8 +33,10 @@ public class EmployeeWage {
 				break;
 
 			}
+			totalEmpHr += empHrs;
 			int empWage = empHrs * RATE_PER_HR;
 			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("Total employee hors: " + totalEmpHr);
 			System.out.println("Emp Wage: " + empWage);
 			System.out.println("Total Emp Wage:" + totalEmpWage);
 		}
